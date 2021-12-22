a8b79dc8e94381828ce87e66c89e95e6eac61061
wangtao <jerwang@mail.ustc.edu.cn>
2021-12-22 09:23:29 +0800

    [add] vimrc配置上传

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+git clone https://github.com/vim-airline/vim-airline.git  ~/.vim/pack/vendor/start/vim-airline
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
+
+git clone https://github.com/preservim/tagbar.git  ~/.vim/pack/vendor/start/tagbar
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/tagbar/doc" -c q
